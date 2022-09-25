31086eeaefd90bcfd85f6eb046c606ae39bf5979
tsuruta <naritakundono@gmail.com>
2022-09-25 11:40:11 +0000

    [add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
     end
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
